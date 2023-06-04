6377220118bc496594774df0758135ece0e07b54
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2023-06-04 10:01:56 +0500

    frontend is ready for deploy

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,6 @@
 
 // export const SERVER_URL = "http://localhost:5000/"
-export const SERVER_URL = "https://adminpaneluz.cyclic.app/";
-export const LOGIN_URL = `${SERVER_URL}api/auth`;
-export const REGISTER_URL = `${SERVER_URL}api/users`;
+export const SERVER_URL = "https://task6-backend.onrender.com/";
+export const GET_MESSAGES = `${SERVER_URL}api/users/me`;
+export const SEND_MASSAGE = `${SERVER_URL}api/users`;
+export const GET_NAMES = `${SERVER_URL}api/names`;
